5dc4c2a2b4cef3fc8feaf56718d8e0bc763536dc
BacchusKC <bacchuskc@gmail.com>
2019-03-12 21:42:23 -0700

    test

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Pennylane23!",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "test",
+    password: "password",
+    database: "burgers_db"
+  });
+};
 
 connection.connect(function(err) {
   if (err) {
